c5af4d6e735092fcd84bb48fc2fd14d9192254ff
Mihai Ciumeica <mihai@thirdhash.com>
2019-01-02 11:59:12 +0100

    Test big- and little-endian serialization
    Signed-off-by: Mihai Ciumeica <mihai@thirdhash.com>

--- src/test.cpp
+++ src/test.cpp
@@ -30,6 +30,19 @@ int main() {
   result = ApduInit(&apdu, 1, 2, 3, 4, buffer, 200);
   assert(result == APDU_OK);
 
+  // Test little- and big-endian serialization
+  uint32_t value = 0x0a0b0c0d;
+  uint8_t big_endian[] { 0x0a, 0x0b, 0x0c, 0x0d };
+  ApduWrite(&apdu, "D", value);
+  assert(memcmp(apdu.buffer + 5, big_endian, 4) == 0);
+
+  ApduInit(&apdu, 1, 2, 3, 4, buffer, 200);
+  uint8_t little_endian[] { 0x0d, 0x0c, 0x0b, 0x0a };
+  ApduWrite(&apdu, "d", value);
+  assert(memcmp(apdu.buffer + 5, little_endian, 4) == 0);
+
+  // Test length calculation, hex and raw binary serialization
+  ApduInit(&apdu, 1, 2, 3, 4, buffer, 200);
   uint8_t bytes[]{0x01, 0x02, 0x03};
   result = ApduWrite(&apdu, "DXr", 0xcafebabe, "deadbeef", (size_t)2, bytes);
   assert(result == 10);
